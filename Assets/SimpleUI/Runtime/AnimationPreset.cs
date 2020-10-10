8d865d12e5fd82494b5da3d05473dd9a2043f438
Sunrise Hirame <benhard.miettinen@gmail.com>
2020-10-10 18:02:35 +0300

    Initial commit

---
+++ Assets/SimpleUI/Runtime/AnimationPreset.cs
@@ -0,0 +1,60 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace SunriseHirame.SimpleUI
+{
+    [CreateAssetMenu (menuName = "Simple UI/Animation Preset")]
+    public class AnimationPreset : ScriptableObject
+    {
+        [Header ("Position")]
+        [SerializeField] private TweenDefinition positionTween = default;
+        
+        [Header ("Rotation")]
+        [SerializeField] private TweenDefinition rotationTween = default;
+        
+        [Header ("Scale")]
+        [SerializeField] private TweenDefinition scaleTween = default;
+        
+        [Header ("Color")]
+        [SerializeField] private TweenDefinition colorTween = default;
+        
+        public async UniTask AnimateIn (RectTransform target, CanvasGroup canvasGroup)
+        {
+            var sequence = DOTween.Sequence ();
+            
+            var targetScale = target.localScale;
+            target.localScale = Vector3.zero;
+            var scale = target.DOScale (targetScale, scaleTween.Duration);
+            scale.SetEase (scaleTween.EaseMode);
+            sequence.Join (scale);
+
+            if (canvasGroup)
+            {
+                var targetColor = canvasGroup.alpha;
+                canvasGroup.alpha = 0;
+                var color = canvasGroup.DOFade (targetColor, colorTween.Duration);
+                color.SetEase (colorTween.EaseMode);
+                sequence.Join (color);   
+            }
+
+            await sequence.AsyncWaitForCompletion ();
+        }
+        
+        [System.Serializable]
+        private struct TweenDefinition
+        {
+            public bool Enabled;
+            public float Duration;
+            public Ease EaseMode;
+            
+            public static TweenDefinition Default => new TweenDefinition
+            {
+                Enabled = false,
+                Duration = 0.7f,
+                EaseMode = Ease.InOutSine
+            };
+        }
+        
+    }
+}
---
+++ Assets/SimpleUI/Runtime/UIView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace SunriseHirame.SimpleUI
+{
+    [RequireComponent (typeof (RectTransform), typeof (CanvasGroup))]
+    public class UIView : MonoBehaviour
+    {
+        [SerializeField] private AnimationPreset inAnimation = default;
+        [SerializeField] private AnimationPreset outAnimation = default;
+        [SerializeField] private AnimationPreset loopAnimation = default;
+
+        [SerializeField] private RectTransform rectTransform = default;
+        [SerializeField] private CanvasGroup canvasGroup = default;
+        
+        private void OnEnable ()
+        {
+            Show ();
+        }
+
+        public void Show ()
+        {
+            if (inAnimation)
+                inAnimation.AnimateIn (rectTransform, canvasGroup);
+        }
+
+        private void Reset ()
+        {
+            rectTransform = GetComponent<RectTransform> ();
+            canvasGroup = GetComponent<CanvasGroup> ();
+        }
+    }
+}
