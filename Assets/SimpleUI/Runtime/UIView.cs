b141a35d4c98082cb3b65b026c19cd1d4dcf6559
Sunrise Hirame <benhard.miettinen@gmail.com>
2020-10-11 17:20:32 +0300

    Update UIView.cs

--- Assets/SimpleUI/Runtime/UIView.cs
+++ Assets/SimpleUI/Runtime/UIView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SunriseHirame.SimpleUI
 {
@@ -7,11 +8,12 @@ namespace SunriseHirame.SimpleUI
 
     public enum UIState
     {
-        Visible,
+        UnInitialized,
+        Active,
         Showing,
         Hidden,
         Hiding,
-        //Disabled
+        Disabled
     }
     
     [RequireComponent (typeof (RectTransform), typeof (CanvasGroup))]
@@ -20,19 +22,23 @@ namespace SunriseHirame.SimpleUI
         public event UIStateAction ShowStarted;
         public event UIStateAction HideStarted;
         
-        [SerializeField] private AnimationPreset inAnimation = default;
-        [SerializeField] private AnimationPreset outAnimation = default;
-        [SerializeField] private AnimationPreset loopAnimation = default;
+        [SerializeField] private TweenPreset inAnimation = default;
+        [SerializeField] private TweenPreset outAnimation = default; 
+        [SerializeField] private TweenPreset loopAnimation = default;
 
         [SerializeField] private RectTransform rectTransform = default;
         [SerializeField] private CanvasGroup canvasGroup = default;
 
-        private UIState state;
+        private UIState state = UIState.UnInitialized;
+
+        public bool IsVisible => state == UIState.Showing || state == UIState.Active || state == UIState.Hiding;
         
         public void Show ()
         {
-            // if (state == UIState.Visible || state == UIState.Showing)
-            //     return;
+            if (state == UIState.Active || state == UIState.Showing)
+                return;
+
+            state = UIState.Showing;
             
             if (gameObject.activeSelf == false)
                 gameObject.SetActive (true);
@@ -42,12 +48,17 @@ namespace SunriseHirame.SimpleUI
             
             if (inAnimation)
                 inAnimation.AnimateIn (rectTransform, canvasGroup);
+
+            state = UIState.Active;
         }
 
         public void Hide ()
         {
-            // if (state == UIState.Hidden || state == UIState.Hiding)
-            //     return;
+            if (state == UIState.Hidden || state == UIState.Hiding)
+                return;
+
+            state = UIState.Hiding;
+            
             HideStarted?.Invoke (this);
             
             gameObject.SetActive (false);
