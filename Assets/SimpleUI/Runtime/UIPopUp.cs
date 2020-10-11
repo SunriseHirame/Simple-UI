b2c96afe27c8e5604af81c953dce7bf39efe5d6a
Sunrise Hirame <benhard.miettinen@gmail.com>
2020-10-11 17:19:23 +0300

    Added basic for pop ups

---
+++ Assets/SimpleUI/Runtime/UIPopUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SunriseHirame.SimpleUI
+{
+    public class UIPopUp : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup = default;
+        [SerializeField] private GraphicRaycaster raycaster = default;
+    }
+
+}
---
+++ Assets/SimpleUI/Runtime/UIPopUpReference.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace SunriseHirame.SimpleUI
+{
+    [CreateAssetMenu (menuName = "Simple UI/Pop Up Reference")]
+    public class UIPopUpReference : ScriptableObject
+    {
+        [SerializeField] private UIPopUp popUpProto = default;
+
+        public UIPopUp ShowPopUp ()
+        {
+            return Instantiate (popUpProto);
+        }
+    }
+}
