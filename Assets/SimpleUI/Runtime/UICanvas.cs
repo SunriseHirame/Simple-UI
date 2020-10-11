323364e6663d3acf51a27f1feed8e0efdeaff5d1
Sunrise Hirame <benhard.miettinen@gmail.com>
2020-10-11 17:19:45 +0300

    Added UICanvas and runtime database for them

---
+++ Assets/SimpleUI/Runtime/UICanvas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SunriseHirame.SimpleUI
+{
+    public class UICanvas : MonoBehaviour
+    {
+        [SerializeField] private string canvasId = "Default";
+        public string Id => canvasId;
+
+        private void OnEnable ()
+        {
+            UICanvasDatabase.RegisterActiveCanvas (this);
+        }
+
+        private void OnDisable ()
+        {
+            UICanvasDatabase.UnregisterActiveCanvas (this);
+        }
+    }
+
+}
---
+++ Assets/SimpleUI/Runtime/UICanvasDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SunriseHirame.SimpleUI
+{
+    public static class UICanvasDatabase
+    {
+        private static readonly Dictionary<string, UICanvas> CanvasMap = new Dictionary<string, UICanvas> ();
+
+        public static bool TryGetUICanvas (string id, out UICanvas uiCanvas)
+        {
+            return CanvasMap.TryGetValue (id, out uiCanvas) && uiCanvas;
+        }
+        
+        internal static void RegisterActiveCanvas (UICanvas uiCanvas)
+        {
+            CanvasMap[uiCanvas.Id] = uiCanvas;
+        }
+
+        internal static void UnregisterActiveCanvas (UICanvas uiCanvas)
+        {
+            CanvasMap.Remove (uiCanvas.Id);
+        }
+    }
+
+}
