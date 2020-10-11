233a94339db76a10f3af50d6210894ed73b61f9c
Sunrise Hirame <benhard.miettinen@gmail.com>
2020-10-11 16:41:55 +0300

    Added simple ui group

--- Assets/SimpleUI/Runtime/UIView.cs
+++ Assets/SimpleUI/Runtime/UIView.cs
@@ -3,27 +3,56 @@ using UnityEngine;
 
 namespace SunriseHirame.SimpleUI
 {
+    public delegate void UIStateAction (UIView view);
+
+    public enum UIState
+    {
+        Visible,
+        Showing,
+        Hidden,
+        Hiding,
+        //Disabled
+    }
+    
     [RequireComponent (typeof (RectTransform), typeof (CanvasGroup))]
     public class UIView : MonoBehaviour
     {
+        public event UIStateAction ShowStarted;
+        public event UIStateAction HideStarted;
+        
         [SerializeField] private AnimationPreset inAnimation = default;
         [SerializeField] private AnimationPreset outAnimation = default;
         [SerializeField] private AnimationPreset loopAnimation = default;
 
         [SerializeField] private RectTransform rectTransform = default;
         [SerializeField] private CanvasGroup canvasGroup = default;
-        
-        private void OnEnable ()
-        {
-            Show ();
-        }
 
+        private UIState state;
+        
         public void Show ()
         {
+            // if (state == UIState.Visible || state == UIState.Showing)
+            //     return;
+            
+            if (gameObject.activeSelf == false)
+                gameObject.SetActive (true);
+            
+            Debug.Log ("SHOW");
+            ShowStarted?.Invoke (this);
+            
             if (inAnimation)
                 inAnimation.AnimateIn (rectTransform, canvasGroup);
         }
 
+        public void Hide ()
+        {
+            // if (state == UIState.Hidden || state == UIState.Hiding)
+            //     return;
+            HideStarted?.Invoke (this);
+            
+            gameObject.SetActive (false);
+        }
+        
         private void Reset ()
         {
             rectTransform = GetComponent<RectTransform> ();
---
+++ Assets/SimpleUI/Runtime/UIViewGroup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace SunriseHirame.SimpleUI
+{
+    public class UIViewGroup : MonoBehaviour
+    {
+        [SerializeField] private UIView[] controlledViews = default;
+        
+        private void Awake ()
+        {
+            foreach (var view in controlledViews)
+            {
+                view.ShowStarted += OnShowStarted;
+            }
+            
+            controlledViews[0].Show ();
+        }
+
+        private void OnShowStarted (UIView shownView)
+        {
+            foreach (var view in controlledViews)
+            {
+                if (shownView != view)
+                    view.Hide ();
+            }
+        }
+    }
+
+}
